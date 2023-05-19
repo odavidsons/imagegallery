8384aa6cd9b2c77bffcd3cb55da0054346ce9df7
odavidsons <davidsantos2807@gmail.com>
2023-05-19 12:21:26 +0100

    new_db

---
+++ SQL/imagegallery230519_dump.sql
@@ -0,0 +1,303 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.7 (Ubuntu 14.7-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.7 (Ubuntu 14.7-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: images; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.images (
+    id integer NOT NULL,
+    name text,
+    path text,
+    uploaded_by text,
+    upload_date text DEFAULT CURRENT_TIMESTAMP(0),
+    description text
+);
+
+
+ALTER TABLE public.images OWNER TO postgres;
+
+--
+-- Name: images_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.images_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.images_id_seq OWNER TO postgres;
+
+--
+-- Name: images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.images_id_seq OWNED BY public.images.id;
+
+
+--
+-- Name: sessions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sessions (
+    id integer NOT NULL,
+    userid text,
+    session text,
+    expires text
+);
+
+
+ALTER TABLE public.sessions OWNER TO postgres;
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.sessions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.sessions_id_seq OWNER TO postgres;
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
+
+
+--
+-- Name: userinfo; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.userinfo (
+    id integer NOT NULL,
+    username text,
+    password text,
+    join_date text DEFAULT CURRENT_TIMESTAMP(0),
+    type integer DEFAULT 0
+);
+
+
+ALTER TABLE public.userinfo OWNER TO postgres;
+
+--
+-- Name: userinfo_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.userinfo_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.userinfo_id_seq OWNER TO postgres;
+
+--
+-- Name: userinfo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.userinfo_id_seq OWNED BY public.userinfo.id;
+
+
+--
+-- Name: userstats; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.userstats (
+    id integer NOT NULL,
+    total_uploaded integer DEFAULT 0,
+    active_uploaded integer DEFAULT 0,
+    userid integer NOT NULL
+);
+
+
+ALTER TABLE public.userstats OWNER TO postgres;
+
+--
+-- Name: userstats_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.userstats_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.userstats_id_seq OWNER TO postgres;
+
+--
+-- Name: userstats_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.userstats_id_seq OWNED BY public.userstats.id;
+
+
+--
+-- Name: images id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.images_id_seq'::regclass);
+
+
+--
+-- Name: sessions id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
+
+
+--
+-- Name: userinfo id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.userinfo ALTER COLUMN id SET DEFAULT nextval('public.userinfo_id_seq'::regclass);
+
+
+--
+-- Name: userstats id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.userstats ALTER COLUMN id SET DEFAULT nextval('public.userstats_id_seq'::regclass);
+
+
+--
+-- Data for Name: images; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.images (id, name, path, uploaded_by, upload_date, description) FROM stdin;
+\.
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sessions (id, userid, session, expires) FROM stdin;
+\.
+
+
+--
+-- Data for Name: userinfo; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.userinfo (id, username, password, join_date, type) FROM stdin;
+17	admin	123	2023-05-19 12:19:43+01	0
+\.
+
+
+--
+-- Data for Name: userstats; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.userstats (id, total_uploaded, active_uploaded, userid) FROM stdin;
+3	0	0	17
+\.
+
+
+--
+-- Name: images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.images_id_seq', 34, true);
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
+
+
+--
+-- Name: userinfo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.userinfo_id_seq', 17, true);
+
+
+--
+-- Name: userstats_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.userstats_id_seq', 3, true);
+
+
+--
+-- Name: images images_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.images
+    ADD CONSTRAINT images_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: userinfo userinfo_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.userinfo
+    ADD CONSTRAINT userinfo_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: userstats userstats_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.userstats
+    ADD CONSTRAINT userstats_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: userstats fk_userid_userinfo; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.userstats
+    ADD CONSTRAINT fk_userid_userinfo FOREIGN KEY (userid) REFERENCES public.userinfo(id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
